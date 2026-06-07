607cd4fb146212c0cbb00006ae6437a3d9e637f2
Andrija Mitrovic <andrijamitrovic11@gmail.com>
2026-06-07 23:41:30 +0200

    update portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,8 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1]
-const isUserPage = repositoryName?.endsWith(".github.io")
-const base = process.env.GITHUB_ACTIONS && repositoryName
-  ? isUserPage
-    ? "/"
-    : `/${repositoryName}/`
-  : "/"
-
 export default defineConfig({
-  base,
+  base: "/portfolio/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
